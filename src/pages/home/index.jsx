77586eb7ae71cd39faae8e4c35171b0cdc48357b
muhcandraajitn <muh.candraajitn@gmail.com>
2022-11-08 16:54:11 +0700

    Rev1

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -39,16 +39,15 @@ const HomePage = () => {
     <>
       <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
-          <a href="" class="flex items-center">
-            <img
-              src={require("./loopslogo.png")}
-              class="mr-3 h-6 sm:h-9"
-              alt="Loops Logo"
-            />
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Loops
-            </span>
-          </a>
+          <img
+            src={require("./loopslogo.png")}
+            class="mr-3 h-6 sm:h-9"
+            alt="Loops Logo"
+          />
+          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            Loops
+          </span>
+
           <div class="flex items-center">
             <a
               href=""
